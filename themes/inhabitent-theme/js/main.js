4541842c64fd39cf8cd01e220dd721570cf21d79
Mark Valdez <Mark Valdez>
2016-08-30 15:47:05 -0700

    fix and clean nav bar javascript code

--- themes/inhabitent-theme/js/main.js
+++ themes/inhabitent-theme/js/main.js
@@ -1,54 +1,43 @@
 (function($){
 $(document).ready(function(){
-	console.log($);
 
 	$('.search-field').hide();
 
 	// nav bar fading
-	window.addEventListener("scroll", function() {
+	window.onscroll = function() {
 			    if (window.scrollY > 50) {
 			    	$("div.fixed-nav-wrapper").addClass("fixed-nav-scroll");
-			    	// $("div.fixed-nav-wrapper").removeClass("fixed-nav");
 
 			    }
 			    else {
 			    	$("div.fixed-nav-wrapper").removeClass("fixed-nav-scroll");
-			    	// $("div.fixed-nav-wrapper").addClass("fixed-nav");
 			    };
-			},false);
+			},false;
 
-	// nav bar fading front about page
-	window.addEventListener("scroll", function() {
+	// nav bar fading front about adventure
+	window.onscroll = function() {
 			    if (window.scrollY > window.screen.height) {
 			    	$("div.front-about-nav").addClass("fixed-front-about-nav-scroll");
 			    	$(".front-about-main-nav").removeClass("white-nav-links");
-			    	$(".front-about-logo").removeClass("white-logo");
-			    	$(".front-about-logo").addClass("green-logo");
-			    	$(".fp-fa-search").removeClass("fa-search-white");
-			    	$(".fp-fa-search").addClass("fa-search-green");
-			    	$(".fp-search-field").removeClass("search-field-white");
-			    	$(".fp-search-field").addClass("search-field-green");
+			    	$(".front-about-logo").removeClass("white-logo").addClass("green-logo");
+			    	$(".fp-fa-search").removeClass("fa-search-white").addClass("fa-search-green");
+			    	$(".fp-search-field").removeClass("search-field-white").addClass("search-field-green");
 
 			    }
 			    else {
 			    	$("div.front-about-nav").removeClass("fixed-front-about-nav-scroll");
 			    	$(".front-about-main-nav").addClass("white-nav-links");
-			    	$(".front-about-logo").removeClass("green-logo");
-			    	$(".front-about-logo").addClass("white-logo");
-			    	$(".fp-fa-search").removeClass("fa-search-green");
-			    	$(".fp-fa-search").addClass("fa-search-white");
-			    	$(".fp-search-field").removeClass("search-field-green");
-			    	$(".fp-search-field").addClass("search-field-white");
+			    	$(".front-about-logo").removeClass("green-logo").addClass("white-logo");
+			    	$(".fp-fa-search").removeClass("fa-search-green").addClass("fa-search-white");
+			    	$(".fp-search-field").removeClass("search-field-green").addClass("search-field-white");
 			    };
-			},false);
+			},false;
 
 
-	// search roggle
+	// search toggle
 	$('body').on('click', function(event) {
         var searchBar = $('.search-field');
         var searchButton = $('.search-submit');
-
-        console.log(event.target);
 
         if ($(event.target).hasClass("search-submit") || $(event.target).hasClass("fa-search")) {
             searchBar.toggle('slow');
